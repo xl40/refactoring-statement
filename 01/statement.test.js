9fe6c91404ef3b5d9da955ffb620653810b5cf1e
Selvin Xue <xuexuedada@icloud.com>
2022-08-09 07:53:54 +0800

    refactor move file

--- 01/statement.test.js
+++ 01/statement.test.js
@@ -5,7 +5,6 @@ describe("Statement", () => {
         const invoice = [2, 7, 11, 15];
         const play = 9;
         const expected = [0,1];
-        // expect(statement(invoice, play)).toBe(expected)
         expect(statement(invoice, play)).toStrictEqual(expected)
     });
 })
