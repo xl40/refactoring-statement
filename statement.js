f5a034cff179f2e7c35a2f24a607d29ab123cfbc
Selvin Xue <xuexuedada@icloud.com>
2022-08-08 21:08:33 +0800

    Test "hello word"

--- statement.js
+++ statement.js
@@ -1,43 +1,12 @@
-function statement(invoice, plays) {
-  let totalAmount = 0;
-  let volumeCredits = 0;
-  let result = `Statement for ${invoice.customer}\n`;
-  const format = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 2,
-  }).format;
-  for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
-    let thisAmount = 0;
-    switch (play.type) {
-    case 'tragedy':
-      thisAmount = 40000;
-      if (perf.audience > 30) {
-        thisAmount += 1000 * (perf.audience - 30);
-      }
-      break;
-    case 'comedy':
-      thisAmount = 30000;
-      if (perf.audience > 20) {
-        thisAmount += 10000 + 500 * (perf.audience - 20);
-      }
-      thisAmount += 300 * perf.audience;
-      break;
-    default:
-      throw new Error(`unknown type: ${play.type}`);
+function statement(nums, target) {
+    for (let i = 0, len = nums.length; i < len; i++) {
+        for (let j = i + 1; j < len; i++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j];
+            }
+        }
     }
-    // add volume credits
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    // add extra credit for every ten comedy attendees
-    if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5);
-    // print line for this order
-    result += `  ${play.name}: ${format(thisAmount / 100)} (${ perf.audience } seats)\n`;
-    totalAmount += thisAmount;
-  }
-  result += `Amount owed is ${format(totalAmount / 100)}\n`;
-  result += `You earned ${volumeCredits} credits\n`;
-  return result;
+
 }
 
 module.exports = statement;
--- statement.test.js
+++ statement.test.js
@@ -2,25 +2,10 @@ const statement = require("./statement")
 
 describe("Statement", () => {
     it('should align baseline', function () {
-        const invoice = {
-            "customer": "BigCo",
-            "performances": [{"playID": "hamlet", "audience": 55}, {
-                "playID": "as-like",
-                "audience": 35
-            }, {"playID": "othello", "audience": 40}]
-        };
-        const play = {
-            "hamlet": {"name": "Hamlet", "type": "tragedy"},
-            "as-like": {"name": "As You Like It", "type": "comedy"},
-            "othello": {"name": "Othello", "type": "tragedy"}
-        };
-
-        const expected = "Statement for BigCo\n" +
-            "  Hamlet: $650.00 (55 seats)\n" +
-            "  As You Like It: $580.00 (35 seats)\n" +
-            "  Othello: $500.00 (40 seats)\n" +
-            "Amount owed is $1,730.00\n" +
-            "You earned 47 credits\n"
-        expect(statement(invoice, play)).toBe(expected)
+        const invoice = [2, 7, 11, 15];
+        const play = 9;
+        const expected = [0,1];
+        // expect(statement(invoice, play)).toBe(expected)
+        expect(statement(invoice, play)).toStrictEqual(expected)
     });
 })
