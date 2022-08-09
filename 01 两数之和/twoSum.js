620cf93a419cfb1190465d14efe48a30f3329918
Selvin Xue <xuexuedada@icloud.com>
2022-08-09 09:48:10 +0800

    add note.

--- 01 两数之和/twoSum.js
+++ 01 两数之和/twoSum.js
@@ -1,21 +1,27 @@
 function twoSum(nums, target) {
-    /*
-    for (let i = 0, len = nums.length; i < len; i++) {
-        for (let j = i + 1; j < len; j++) {
-            if (nums[i] + nums[j] === target) {
-                return [i, j];
-            }
-        }
-    }
-    return [-1, -1];
-    */
+    /**
+     *  暴破
+     */
 
+    // for (let i = 0, len = nums.length; i < len; i++) {
+    //     for (let j = i + 1; j < len; j++) {
+    //         if (nums[i] + nums[j] === target) {
+    //             return [i, j];
+    //         }
+    //     }
+    // }
+    // return [-1, -1];
+
+
+    /**
+     *  哈希表
+     */
     const map = new Map();
     for (let i = 0, len = nums.length; i < len; i++) {
         if (map.has(target - nums[i])) {
             return [map.get(target - nums[i]), i]
         } else {
-            map.set(nums[i], i);
+            map.set(nums[i], i)
         }
     }
 
