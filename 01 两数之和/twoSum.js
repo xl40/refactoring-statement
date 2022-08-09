482366d7980cc9821e0472d07a87f6637efa2ecd
Selvin Xue <xuexuedada@icloud.com>
2022-08-09 08:23:00 +0800

    fix

--- 01 两数之和/twoSum.js
+++ 01 两数之和/twoSum.js
@@ -1,11 +1,13 @@
 function twoSum(nums, target) {
     for (let i = 0, len = nums.length; i < len; i++) {
-        for (let j = i + 1; j < len; i++) {
+        for (let j = i + 1; j < len; j++) {
             if (nums[i] + nums[j] === target) {
                 return [i, j];
             }
         }
     }
+    // 所有样例都是有返回结果的，这里无所谓
+    return [-1, -1];
 
 }
 
--- 01 两数之和/twoSum.test.js
+++ 01 两数之和/twoSum.test.js
@@ -7,4 +7,17 @@ describe("Statement", () => {
         const expected = [0,1];
         expect(twoSum(invoice, play)).toStrictEqual(expected)
     });
+    it('should return 2', function () {
+        const invoice = [3, 2, 4];
+        const play = 6;
+        const expected = [1,2];
+        expect(twoSum(invoice, play)).toStrictEqual(expected)
+    })
+    it('should return 3', function () {
+        const invoice = [3, 3];
+        const play = 6;
+        const expected = [0, 1];
+        expect(twoSum(invoice, play)).toStrictEqual(expected)
+    })
+
 })
